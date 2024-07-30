5b161348d97b554062ea569325b7e0a6680997bd
fagneraureliano <fagner2aureliano@gmail.com>
2024-07-30 10:41:06 -0300

    feat: :construction: impl model, repository and continue config consumer

--- src/main/java/com/ms/email/consumers/EmailConsumer.java
+++ src/main/java/com/ms/email/consumers/EmailConsumer.java
@@ -1,17 +1,20 @@
 package com.ms.email.consumers;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 import com.ms.email.dtos.EmailRecordDTO;
+import com.ms.email.models.EmailModel;
 
 @Component
 public class EmailConsumer {
 
     @RabbitListener(queues = "${broker.queue.email.name}")
-    public void listenEmailQueue(@Payload EmailRecordDTO email) {
-        System.out.println("Email: " + email.emailTo());
+    public void listenEmailQueue(@Payload EmailRecordDTO dto) {
+        var emailModel = new EmailModel();
+        BeanUtils.copyProperties(dto, emailModel); 
     }
 
 }
