d2d7aea1f47b2d314dd089e830983a0c85e04a59
fagneraureliano <fagner2aureliano@gmail.com>
2024-07-30 19:05:21 -0300

    feat: :construction: continue impl service and consumer

--- src/main/java/com/ms/email/consumers/EmailConsumer.java
+++ src/main/java/com/ms/email/consumers/EmailConsumer.java
@@ -7,14 +7,22 @@ import org.springframework.stereotype.Component;
 
 import com.ms.email.dtos.EmailRecordDTO;
 import com.ms.email.models.EmailModel;
+import com.ms.email.services.EmailService;
 
 @Component
 public class EmailConsumer {
+
+    final EmailService emailService;
+
+    public EmailConsumer(EmailService emailService) {
+        this.emailService = emailService;
+    }
 
     @RabbitListener(queues = "${broker.queue.email.name}")
     public void listenEmailQueue(@Payload EmailRecordDTO dto) {
         var emailModel = new EmailModel();
         BeanUtils.copyProperties(dto, emailModel); 
+        emailService.sendEmail(emailModel);
     }
 
 }
